a9f6e76bc31ccec9f25fe60b4ff00ba481ad3886
Jeremy Bischoff <jbisc100@gmail.com>
2024-07-06 17:30:04 -0700

    Manually added average scores for efficiency on load

--- server/main.py
+++ server/main.py
@@ -16,6 +16,7 @@ from collections import defaultdict
 
 eventKey = ["FX", "PH", "SR", "V", "PB", "HB"]
 
+"""
 champsURL = "https://myusagym.com/meets/past/89744/results/256676/" 
 trialsURL = "https://myusagym.com/meets/past/90017/results/256770/"
 
@@ -79,6 +80,169 @@ def calculateAverageFourScores(data):
 averagedScores = calculateAverageFourScores(gymnastData)
 
 initial_scores = averagedScores
+"""
+initial_scores = {
+  "Alex Diab": [
+    0.0,
+    0.0,
+    14.625,
+    0.0,
+    0.0,
+    0.0
+  ],
+  "Asher Hong": [
+    13.975,
+    12.312,
+    14.55,
+    14.537,
+    14.762,
+    13.025
+  ],
+  "Brody Malone": [
+    13.925,
+    13.875,
+    14.45,
+    14.425,
+    14.738,
+    14.238
+  ],
+  "Cameron Bock": [
+    13.738,
+    13.975,
+    13.775,
+    13.75,
+    14.225,
+    13.65
+  ],
+  "Colt Walker": [
+    13.562,
+    13.662,
+    13.738,
+    14.062,
+    14.863,
+    12.613
+  ],
+  "Curran Phillips": [
+    9.75,
+    0.0,
+    0.0,
+    10.25,
+    11.688,
+    9.9
+  ],
+  "Donnell Whittenburg": [
+    14.175,
+    12.887,
+    14.425,
+    14.412,
+    14.3,
+    12.975
+  ],
+  "Fred Richard": [
+    14.55,
+    13.3,
+    13.988,
+    14.25,
+    14.688,
+    14.412
+  ],
+  "Fuzzy Benas": [
+    13.988,
+    13.7,
+    13.537,
+    13.863,
+    14.075,
+    13.463
+  ],
+  "Jeremy Bischoff": [
+    13.688,
+    13.213,
+    13.338,
+    13.838,
+    13.775,
+    13.238
+  ],
+  "Joshua Karnes": [
+    6.487,
+    12.938,
+    6.612,
+    6.1,
+    14.463,
+    13.15
+  ],
+  "Kai Uemura": [
+    12.963,
+    11.575,
+    12.912,
+    13.3,
+    13.25,
+    12.812
+  ],
+  "Khoi Young": [
+    14.088,
+    13.375,
+    9.95,
+    14.925,
+    14.688,
+    13.162
+  ],
+  "Kiran Mandava": [
+    13.05,
+    12.4,
+    12.875,
+    13.725,
+    13.475,
+    12.7
+  ],
+  "Patrick Hoopes": [
+    0.0,
+    14.675,
+    0.0,
+    0.0,
+    0.0,
+    0.0
+  ],
+  "Paul Juda": [
+    14.325,
+    14.05,
+    13.887,
+    14.287,
+    14.025,
+    13.425
+  ],
+  "Shane Wiskus": [
+    14.387,
+    13.637,
+    13.838,
+    14.375,
+    14.613,
+    13.613
+  ],
+  "Stephen Nedoroscik": [
+    0.0,
+    14.825,
+    0.0,
+    0.0,
+    0.0,
+    0.0
+  ],
+  "Tate Costa": [
+    12.613,
+    10.988,
+    11.938,
+    13.525,
+    12.8,
+    12.55
+  ],
+  "Yul Moldauer": [
+    14.3,
+    13.062,
+    13.938,
+    13.95,
+    15.012,
+    13.113
+  ]
+}
 
 # team scenario class
 class TeamScenario:
